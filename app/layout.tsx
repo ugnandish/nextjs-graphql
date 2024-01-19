44819b733f839e19f8ef39b15c013bfe994378dd
nandish.ug <nandish.ug@gmail.com>
2024-01-19 17:13:48 +0000

    nextjs-graphql

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
+import { ClerkProvider, SignIn, SignedIn, SignedOut } from '@clerk/nextjs' 
+import NavBar from '@/components/NavBar'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Raleway } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Raleway({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +17,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <SignedIn>
+            <NavBar />
+            {children}
+          </SignedIn>
+          <SignedOut>
+            <SignIn />
+          </SignedOut>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
